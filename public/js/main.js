458c5433f0ecab85ad5a814adce28ebe09fe6644
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-09-29 00:26:56 +0200

    Added functions to update cart price totals

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,6 @@ if (document.readyState == 'loading') {
 }
 
 //Ready Function
-
 function ready() {
   // Remove Items from Cart
   var removeCart = document.getElementsByClassName('cart-remove')
@@ -28,10 +27,45 @@ function ready() {
     var button = removeCart[i]
     button.addEventListener('click', removeCartItem)
   }
+  //Item Update
+  var itemsUpdate = document.getElementsByClassName('items')
+  for (var i = 0; i < itemsUpdate.length; i++) {
+    var input = itemsUpdate[i]
+    input.addEventListener('change', itemsChanged)
+  }
+  // Add to Cart
 }
 
 // Remove Cart Item
 function removeCartItem(event) {
   var buttonClicked = event.target
   buttonClicked.parentElement.remove()
+  updateTotal()
+}
+
+// Item Quantity updated
+function itemsChanged(event) {
+  var input = event.target
+  if (isNaN(input.value) || input.value <= 0) {
+    // Display an error message or handle the invalid input as needed
+    console.log('Invalid input. Please enter a valid quantity.')
+    input.value = 1
+  }
+  updateTotal()
+}
+
+// Update Total
+function updateTotal() {
+  var cartBoxes = document.querySelectorAll('.cart-box')
+  var total = 0
+
+  cartBoxes.forEach(function (cartBox) {
+    var priceElement = cartBox.querySelector('.card-price')
+    var itemsElement = cartBox.querySelector('.items')
+    var price = parseFloat(priceElement.innerText.replace('kr', ''))
+    var item = parseFloat(itemsElement.value)
+    total += price * item
+  })
+
+  document.querySelector('.total-price').innerText = 'kr' + total.toFixed(2) // Format total with two decimal places
 }
