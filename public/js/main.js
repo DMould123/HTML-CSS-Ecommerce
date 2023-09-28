18e04a1c1398e6ef5ce1ef9c236550eaac537af2
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-09-28 23:30:43 +0200

    Added a Remove Item function from the cart

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,27 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
   cart.classList.remove('cart-active')
 }
+
+// Add to Cart
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready()
+}
+
+//Ready Function
+
+function ready() {
+  // Remove Items from Cart
+  var removeCart = document.getElementsByClassName('cart-remove')
+  for (var i = 0; i < removeCart.length; i++) {
+    var button = removeCart[i]
+    button.addEventListener('click', removeCartItem)
+  }
+}
+
+// Remove Cart Item
+function removeCartItem(event) {
+  var buttonClicked = event.target
+  buttonClicked.parentElement.remove()
+}
