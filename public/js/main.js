2da60a8b0c5dc3755f6a0614c2f4ed5d4c180aae
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-09-30 05:47:18 +0200

    Updated Add Cart Function and Update Total Function. Gave all T-Shirts a unique name so that they could be added to cart with no error messsages

--- public/js/main.js
+++ public/js/main.js
@@ -34,6 +34,11 @@ function ready() {
     input.addEventListener('change', itemsChanged)
   }
   // Add to Cart
+  var addCart = document.getElementsByClassName('add-cart')
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i]
+    button.addEventListener('click', addCartClicked)
+  }
 }
 
 // Remove Cart Item
@@ -54,6 +59,52 @@ function itemsChanged(event) {
   updateTotal()
 }
 
+// Add Cart Function
+function addCartClicked(event) {
+  var button = event.target
+  var shopProducts = button.parentElement
+  var title = shopProducts.getElementsByClassName('product-title')[0].innerText
+  var price = shopProducts.getElementsByClassName('price')[0].innerText
+  var productImage = shopProducts
+    .getElementsByClassName('product-img')[0]
+    .getAttribute('src')
+  addProductToCart(title, price, productImage)
+  updateTotal()
+}
+
+function addProductToCart(title, price, productImage) {
+  var cartShopBox = document.createElement('div')
+  cartShopBox.classList.add('cart-box')
+  var cartItems = document.querySelector('.cart-content') // Change to querySelector
+  var cartItemsTitles = cartItems.getElementsByClassName('card-product-title')
+
+  for (var i = 0; i < cartItemsTitles.length; i++) {
+    if (cartItemsTitles[i].innerText === title) {
+      alert('You previously added this item to the cart!')
+      return
+    }
+  }
+
+  var cartBoxContent = `
+    <img src="${productImage}" alt="" class="cart-img">
+    <div class="card-detail-box">
+      <div class="card-product-title">${title}</div>
+      <div class="card-price">${price}</div>
+      <input type="number" name="" id="" value="1" class="items" />
+    </div>
+    <!-- Remove Items from Cart -->
+    <i class="bx bx-trash cart-remove"></i>
+  `
+
+  cartShopBox.innerHTML = cartBoxContent
+  cartItems.appendChild(cartShopBox)
+
+  cartShopBox
+    .querySelector('.cart-remove')
+    .addEventListener('click', removeCartItem) // Corrected querySelector
+  cartShopBox.querySelector('.items').addEventListener('change', itemsChanged) // Corrected querySelector
+}
+
 // Update Total
 function updateTotal() {
   var cartBoxes = document.querySelectorAll('.cart-box')
@@ -62,10 +113,18 @@ function updateTotal() {
   cartBoxes.forEach(function (cartBox) {
     var priceElement = cartBox.querySelector('.card-price')
     var itemsElement = cartBox.querySelector('.items')
-    var price = parseFloat(priceElement.innerText.replace('kr', ''))
-    var item = parseFloat(itemsElement.value)
-    total += price * item
+
+    // Check if priceElement and itemsElement exist
+    if (priceElement && itemsElement) {
+      var price = parseFloat(priceElement.innerText.replace('kr', ''))
+      var item = parseFloat(itemsElement.value)
+      total += price * item
+    }
   })
 
-  document.querySelector('.total-price').innerText = 'kr' + total.toFixed(2) // Format total with two decimal places
+  // Update the total price
+  var totalPriceElement = document.querySelector('.total-price')
+  if (totalPriceElement) {
+    totalPriceElement.innerText = 'kr' + total.toFixed(2) // Format total with two decimal places
+  }
 }
